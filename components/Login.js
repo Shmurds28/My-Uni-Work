2f15e62827873c36a7cd383ee53a554f1292461f
Simamkele Ngcasane <simamkelengcasane28@gmail.com>
2022-08-31 20:36:57 +0100

    Show/Hide password

--- components/Login.js
+++ components/Login.js
@@ -6,6 +6,7 @@ import Router, { useRouter } from "next/router";
 import { db } from '../firebase';
 import { doc, getDoc } from 'firebase/firestore';
 import { LockClosedIcon } from '@heroicons/react/outline';
+import {EyeIcon } from '@heroicons/react/solid';
 
 
 function Login() {
@@ -18,6 +19,7 @@ function Login() {
     const {signIn} = useUserAuth();
     const {user, setUserInfo, userInfo} = useUserAuth(); 
     const router = useRouter();
+    const [showPassword, setShowPassword] = useState(false);
 
     const doLogin = async (e) => {
         e.preventDefault();
@@ -68,6 +70,14 @@ function Login() {
         // Router.reload(window.location.pathname)
     }
 
+    const doShowPassword = () => {
+      setShowPassword(true);
+    }
+
+    const doHidePassword = () => {
+      setShowPassword(false);
+    }
+
   return (
     <div className="">
       <div className=" flex items-center align-center w-14 h-14 mx-auto justify-center p-2 bg-[#103A5C] m-0 rounded-full ">
@@ -96,7 +106,26 @@ function Login() {
              <span className=" block text-sm font-semibold text-slate-700">
                Password
              </span>
-             <input value={password} onChange= {(e) => setPassword(e.target.value)} type="password" className=" rounded-md mt-1 w-full px-3 py-2 bg-white border shadow-sm border-slate-300" />
+             <div className="relative">
+                <div className="absolute flex justify-end w-full right-1 items-center cursor-pointer m-3">
+                  <EyeIcon className={`${!showPassword && "hidden"} w-6 h-6 text-[#000] font-semibold `}
+                      onClick={doHidePassword}/>
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`${showPassword && "hidden"} w-6 h-6 text-[#000] font-semibold `} onClick={doShowPassword}>
+                        <path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" />
+                        <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" />
+                        <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
+                      </svg>
+
+                 </div>
+                <input
+                 value={password} 
+                 onChange= {(e) => setPassword(e.target.value)} 
+                 type={`${showPassword ? "text" : "password"}`} 
+                 className=" rounded-md mt-1 w-full px-3 py-2 bg-white border shadow-sm border-slate-300"
+                />
+                 
+             </div>
+             
           </label>
          </div>
 
--- components/Signup.js
+++ components/Signup.js
@@ -6,6 +6,7 @@ import { useRecoilState } from 'recoil';
 import { modalState, signup } from '../atoms/modalAtom';
 import {useUserAuth } from '../context/UserAuthContext';
 import { db } from '../firebase';
+import {EyeIcon } from '@heroicons/react/solid';
 
 function Signup() {
     const [isOpen, setIsOpen] = useRecoilState(modalState);
@@ -21,6 +22,7 @@ function Signup() {
     const {user, userInfo} = useUserAuth();
     const [modules, setModules] = useState(["WHPV400","WRHV411"]);
     const router = useRouter();
+    const [showPassword, setShowPassword] = useState(false);
 
     const doSignup = async (e) => {
       e.preventDefault();
@@ -61,6 +63,14 @@ function Signup() {
       }
 
       // Router.reload(window.location.pathname)
+    }
+
+    const doShowPassword = () => {
+      setShowPassword(true);
+    }
+
+    const doHidePassword = () => {
+      setShowPassword(false);
     }
 
   return (
@@ -110,7 +120,25 @@ function Signup() {
              <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-semibold text-slate-700">
                Password
              </span>
-             <input value={password} onChange= {(e) => setPassword(e.target.value)} type="password" className=" rounded-md mt-1 w-full px-3 py-2 bg-white border shadow-sm border-slate-300" />
+             <div className="relative">
+                <div className="absolute flex justify-end w-full right-1 items-center cursor-pointer m-3">
+                  <EyeIcon className={`${!showPassword && "hidden"} w-6 h-6 text-[#000] font-semibold `}
+                      onClick={doHidePassword}/>
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`${showPassword && "hidden"} w-6 h-6 text-[#000] font-semibold `} onClick={doShowPassword}>
+                        <path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" />
+                        <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" />
+                        <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
+                      </svg>
+
+                 </div>
+                <input
+                 value={password} 
+                 onChange= {(e) => setPassword(e.target.value)} 
+                 type={`${showPassword ? "text" : "password"}`} 
+                 className=" rounded-md mt-1 w-full px-3 py-2 bg-white border shadow-sm border-slate-300"
+                />
+                 
+             </div>
           </label>
          </div>
 
@@ -119,8 +147,26 @@ function Signup() {
              <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-semibold text-slate-700">
                Confirm Password
              </span>
-             <input value={confirmPassword} onChange= {(e) => setConfirmPassword(e.target.value)} type="password" className=" rounded-md mt-1 w-full px-3 py-2 bg-white border shadow-sm border-slate-300" />
-          </label>
+             <div className="relative">
+                <div className="absolute flex justify-end w-full right-1 items-center cursor-pointer m-3">
+                  <EyeIcon className={`${!showPassword && "hidden"} w-6 h-6 text-[#000] font-semibold `}
+                      onClick={doHidePassword}/>
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`${showPassword && "hidden"} w-6 h-6 text-[#000] font-semibold `} onClick={doShowPassword}>
+                        <path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" />
+                        <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" />
+                        <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
+                      </svg>
+
+                 </div>
+                <input
+                 value={confirmPassword} 
+                 onChange= {(e) => setConfirmPassword(e.target.value)} 
+                 type={`${showPassword ? "text" : "password"}`} 
+                 className=" rounded-md mt-1 w-full px-3 py-2 bg-white border shadow-sm border-slate-300"
+                />
+                 
+             </div>
+            </label>
          </div>
 
          <div className="m-1">
